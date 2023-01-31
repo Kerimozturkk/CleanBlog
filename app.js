8efecab0b67def6993a5714132d65f5cc354f557
Kerim Öztürk <kerim_648@hotmail.com>
2023-01-31 16:53:07 +0300

    posts single pages have been routed

--- app.js
+++ app.js
@@ -5,50 +5,54 @@ const ejs = require('ejs');
 
 const Post = require('./models/Post');
 
-mongoose.connect('mongodb://localhost/cleanblog-test-db',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Template Engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(express.static('public'));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // Routes
-app.get('/',async(req,res) => {
-    const posts = await Post.find({});
-    res.render('index',{
-        posts
-    });
-})
-
-app.get('/about',(req,res) => {
-    res.render('about');
-})
-
-app.get('/add_post',(req,res) => {
-    res.render('add_post');
-})
-
-app.get('/post',(req,res) => {
-    res.render('post');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
 });
 
+app.get('/posts/:id', async(req, res) => {
+    const post = await Post.findById(req.params.id);
+    res.render('post',{
+        post
+    });
+  });
+  
 
-app.post('/posts',async(req,res) => {
-    await Post.create(req.body)
-    res.redirect('/');
-})
+app.get('/about', async(req, res) => {
+  res.render('about');
+});
 
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server port ${port} de başlatıldı...`);
-})
+  console.log(`Server port ${port} de başlatıldı...`);
+});
